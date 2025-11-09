e52d3a66fa82aa87c0ff353e1b707ead0ff2bbb1
Franco Callero <fcallero@frba.utn.edu.ar>
2025-11-09 14:37:05 +0000

    agrego makefile version windows

--- windows/settings.mk
+++ windows/settings.mk
@@ -10,11 +10,11 @@ CDEBUG=-g -Wall -DDEBUG -fcommon -fdiagnostics-color=always
 CRELEASE=-O3 -Wall -DNDEBUG -fcommon
 
 # Arguments when executing with start, memcheck or helgrind
-ARGS=bin/contenido.txt 1234ACB980154D89F1B123F011208965 1 bin/resultado.txt
+ARGS=../bin/contenido.txt 1234ACB980154D89F1B123F011208965 1 ../bin/resultado.txt
 
 # Valgrind flags
 MEMCHECK_FLAGS=--track-origins=yes
 HELGRIND_FLAGS=
 
 # Source files (*.c) to be excluded from tests compilation
-TEST_EXCLUDE=src/main.c
+TEST_EXCLUDE=../src/main.c
